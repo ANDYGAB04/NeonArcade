d1b0dc64c5c55d2734be2dba298f799607a61f0c
Suhan Mihai-Andrei <suhan.mihai.r4q@student.ucv.ro>
2025-11-18 14:23:49 +0200

    Order Method

---
+++ NeonArcade.Server/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace NeonArcade.Server.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string OrderNumber { get; set; } = string.Empty;
+        public DateTime OrderDate { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+
+
+
+
+
+    }
+}
--- NeonArcade.Server/Models/User.cs
+++ NeonArcade.Server/Models/User.cs
@@ -8,5 +8,7 @@ namespace NeonArcade.Server.Models
         public string FirstName { get; set; } = string.Empty;
 
         public string LastName { get; set; } = string.Empty;
+
+        public List<Order>? Orders { get; set; }
     }
 }
