ee661ade66aa91a7d7a2a28851450c6109dd6aa1
Suhan Mihai-Andrei <suhan.mihai.r4q@student.ucv.ro>
2025-11-18 14:15:40 +0200

    UserModel

---
+++ NeonArcade.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace NeonArcade.Server.Models
+{
+    public class User : IdentityUser<int>
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+    }
+}
