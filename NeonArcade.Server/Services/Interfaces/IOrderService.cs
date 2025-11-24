2541d1631b216fa92bb2ba01e6a51e19dc8c3ceb
Suhan Mihai-Andrei <suhan.mihai.r4q@student.ucv.ro>
2025-11-24 18:51:29 +0200

    interface serivce

--- NeonArcade.Server/Services/Interfaces/IOrderService.cs
+++ NeonArcade.Server/Services/Interfaces/IOrderService.cs
@@ -1,6 +1,17 @@
-﻿namespace NeonArcade.Server.Services.Interfaces
+﻿using NeonArcade.Server.Models;
+
+namespace NeonArcade.Server.Services.Interfaces
 {
     public interface IOrderService
     {
+        Task<IEnumerable<Order>> GetOrdersByUserIdAsync(string userId);
+        Task<Order?> GetOrderByIdAsync(int orderId);
+        Task<Order> CreateOrderAsync(Order order);
+        Task<Order> UpdateOrderAsync(int orderId, Order order);
+        Task<bool> DeleteOrderAsync(int orderId);
+        Task<Order> CreateOrderFromCartAsync(string userId);
+        Task<Order?> GetOrderWithDetailsAsync(int orderId);
+
+
     }
 }
